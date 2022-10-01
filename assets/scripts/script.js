987b4ec8aa990e33cc52cfbfd57e15f68f853ef6
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2022-10-01 14:00:56 +0400

    format and fix urls

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,11 @@
 const $time = document.querySelector('.time'),
-  $greeting = document.querySelector('.greeting'),
-  $name = document.querySelector('.name'),
-  $focus = document.querySelector('.focus');
+    $greeting = document.querySelector('.greeting'),
+    $name = document.querySelector('.name'),
+    $focus = document.querySelector('.focus');
 
 const $blockquote = document.querySelector('blockquote'),
-  $figcaption = document.querySelector('figcaption'),
-  $btnQuote = document.querySelector('#btn-quote');
+    $figcaption = document.querySelector('figcaption'),
+    $btnQuote = document.querySelector('#btn-quote');
 
 const $weatherIcon = document.querySelector('.weather-icon');
 const $temperature = document.querySelector('.temperature');
@@ -15,25 +15,25 @@ const $city = document.querySelector('.city');
 const $btnImage = document.querySelector('#btn-change-image');
 
 function showTime() {
-  let today = new Date(),
-    hour = today.getHours(),
-    min = today.getMinutes(),
-    sec = today.getSeconds();
+    let today = new Date(),
+        hour = today.getHours(),
+        min = today.getMinutes(),
+        sec = today.getSeconds();
 
-  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    
-  let day = today.getDate(),
-    dayOfWeek = days[ today.getDay() ],
-    month = months[ today.getMonth() ];
-  $time.innerHTML = `${hour}:${addZeros(min)}:${addZeros(sec)}, <span>${day} ${dayOfWeek} ${month}</span>`;
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-  setTimeout(showTime, 1000);
+    let day = today.getDate(),
+        dayOfWeek = days[today.getDay()],
+        month = months[today.getMonth()];
+    $time.innerHTML = `${hour}:${addZeros(min)}:${addZeros(sec)}, <span>${day} ${dayOfWeek} ${month}</span>`;
+
+    setTimeout(showTime, 1000);
 }
 
-const addZeros = (n) => {return (parseInt(n, 10) < 10 ? '0' : '') + n}
+const addZeros = (n) => { return (parseInt(n, 10) < 10 ? '0' : '') + n }
 
-const randomArr = (arr) => arr.slice(0).sort( (a,b)=> 0.5-Math.random()); 
+const randomArr = (arr) => arr.slice(0).sort((a, b) => 0.5 - Math.random());
 
 let base = '';
 const baseArr = ['./assets/images/morning/', './assets/images/afternoon/', './assets/images/evening/', './assets/images/night/'];
@@ -43,180 +43,180 @@ const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.
 let imagesRandom = [];
 let indextime = 0;
 function setBgGreet() {
-  let today = new Date(),
-    hour = today.getHours();
+    let today = new Date(),
+        hour = today.getHours();
 
-  if (hour >= 6 && hour < 12) {
-    $greeting.textContent = 'Good Morning, ';
-    base = './assets/images/morning/';
-    indextime = 0;
-  } 
-  else if (hour >= 12 && hour < 18) {
-    $greeting.textContent = 'Good Afternoon, ';
-    base = './assets/images/afternoon/';
-    indextime = 1;
-  } 
-  else if (hour >= 18 && hour <= 23) {
-    $greeting.textContent = 'Good Evening, ';
-    base = './assets/images/evening/';
-    indextime = 2;
+    if (hour >= 6 && hour < 12) {
+        $greeting.textContent = 'Good Morning, ';
+        base = './assets/images/morning/';
+        indextime = 0;
+    }
+    else if (hour >= 12 && hour < 18) {
+        $greeting.textContent = 'Good Afternoon, ';
+        base = './assets/images/afternoon/';
+        indextime = 1;
+    }
+    else if (hour >= 18 && hour <= 23) {
+        $greeting.textContent = 'Good Evening, ';
+        base = './assets/images/evening/';
+        indextime = 2;
 
-  } 
-  else {
-    $greeting.textContent = 'Good Night, ';
-    base = './assets/images/night/';
-    indextime = 3;
-    //document.body.style.color = 'azure';
-  }
-  return base;
+    }
+    else {
+        $greeting.textContent = 'Good Night, ';
+        base = './assets/images/night/';
+        indextime = 3;
+        //document.body.style.color = 'azure';
+    }
+    return base;
 }
 setBgGreet(base);
 
 function viewBgImage(data) {
-  const body = document.querySelector('body');
-  const src = data;
-  const img = document.createElement('img');
-  img.src = src;
-  img.onload = () => {      
-    body.style.backgroundImage = `url(${src}), url(./assets/images/overlay.png)`;
-  }; 
+    const body = document.querySelector('body');
+    const src = data;
+    const img = document.createElement('img');
+    img.src = src;
+    img.onload = () => {
+        body.style.backgroundImage = `url(${src}), url(../images/overlay.png)`;
+    };
 }
 let i = 0;
 
 function getImage() {
-  let basedImages = [];
-  if (indextime === 0) {
-    basedImages = [setBg(baseArr[0], images), setBg(baseArr[1], images), setBg(baseArr[2], images), setBg(baseArr[3], images)];
-  }
-  else if (indextime === 1) {
-    basedImages = [setBg(baseArr[1], images), setBg(baseArr[2], images), setBg(baseArr[3], images), setBg(baseArr[0], images)];
-  }
-  else if (indextime === 2) {
-    basedImages = [setBg(baseArr[2], images), setBg(baseArr[3], images), setBg(baseArr[0], images), setBg(baseArr[1], images)];
-  }
-  else if (indextime === 3) {
-    basedImages = [setBg(baseArr[3], images), setBg(baseArr[0], images), setBg(baseArr[1], images), setBg(baseArr[2], images)];
-  }
-  basedImages = basedImages.flat();
-  const index = i % basedImages.length;
-  const imageSrc = `${basedImages[index]}`;
-  viewBgImage(imageSrc);
-  i++;  
-  $btnImage.disabled = true;
-  setTimeout(function() {$btnImage.disabled = false}, 1000);
-} 
+    let basedImages = [];
+    if (indextime === 0) {
+        basedImages = [setBg(baseArr[0], images), setBg(baseArr[1], images), setBg(baseArr[2], images), setBg(baseArr[3], images)];
+    }
+    else if (indextime === 1) {
+        basedImages = [setBg(baseArr[1], images), setBg(baseArr[2], images), setBg(baseArr[3], images), setBg(baseArr[0], images)];
+    }
+    else if (indextime === 2) {
+        basedImages = [setBg(baseArr[2], images), setBg(baseArr[3], images), setBg(baseArr[0], images), setBg(baseArr[1], images)];
+    }
+    else if (indextime === 3) {
+        basedImages = [setBg(baseArr[3], images), setBg(baseArr[0], images), setBg(baseArr[1], images), setBg(baseArr[2], images)];
+    }
+    basedImages = basedImages.flat();
+    const index = i % basedImages.length;
+    const imageSrc = `${basedImages[index]}`;
+    viewBgImage(imageSrc);
+    i++;
+    $btnImage.disabled = true;
+    setTimeout(function () { $btnImage.disabled = false }, 1000);
+}
 
 getImage();
 function checkHour(func) {
-  let today = new Date(),
-  min = today.getMinutes();
+    let today = new Date(),
+        min = today.getMinutes();
 
-  if (min !== 00) {
-    let timeout = (60 - min) * 60000;
-    setTimeout(()=>func(), timeout);
-  }
-  else {
-    func();
-  }
+    if (min !== 00) {
+        let timeout = (60 - min) * 60000;
+        setTimeout(() => func(), timeout);
+    }
+    else {
+        func();
+    }
 }
 
 checkHour(getImage);
 
 function getElem(elem, dom) {
-  if (localStorage.getItem(`${elem}`) === null) {
-    dom.textContent = `[Enter ${elem}]`;
-  } else {
-    dom.textContent = localStorage.getItem(`${elem}`);
-  }
+    if (localStorage.getItem(`${elem}`) === null) {
+        dom.textContent = `[Enter ${elem}]`;
+    } else {
+        dom.textContent = localStorage.getItem(`${elem}`);
+    }
 }
 
 
 function setName(e) {
-  if (e.type === 'keypress') {
+    if (e.type === 'keypress') {
 
-    if (e.which == 13 || e.keyCode == 13) {
-      if (e.target.innerText === '') {
-        e.target.innerText = localStorage.getItem('name');
-        $name.innerText = localStorage.getItem('name');
-      }
-      else {
-        localStorage.setItem('name', e.target.innerText);
-      }
+        if (e.which == 13 || e.keyCode == 13) {
+            if (e.target.innerText === '') {
+                e.target.innerText = localStorage.getItem('name');
+                $name.innerText = localStorage.getItem('name');
+            }
+            else {
+                localStorage.setItem('name', e.target.innerText);
+            }
 
-      $name.blur();
+            $name.blur();
+        }
     }
-  } 
 }
 
 function setFocus(e) {
-  if (e.type === 'keypress') {
+    if (e.type === 'keypress') {
 
-    if (e.which == 13 || e.keyCode == 13) {
-      if (e.target.innerText === '') {
-        e.target.innerText = localStorage.getItem('focus');
-        $focus.innerText = localStorage.getItem('focus');
-      }
-      else {
-        localStorage.setItem('focus', e.target.innerText);
-      }
-      
-      $focus.blur();
+        if (e.which == 13 || e.keyCode == 13) {
+            if (e.target.innerText === '') {
+                e.target.innerText = localStorage.getItem('focus');
+                $focus.innerText = localStorage.getItem('focus');
+            }
+            else {
+                localStorage.setItem('focus', e.target.innerText);
+            }
+
+            $focus.blur();
+        }
     }
-  }
 }
 
-async function getQuote() {  
-  const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
-  const res = await fetch(url);
-  const quote = await res.json(); 
-  $blockquote.textContent = quote.quoteText;
-  $figcaption.textContent = quote.quoteAuthor;
+async function getQuote() {
+    const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en`;
+    const res = await fetch(url);
+    const quote = await res.json();
+    $blockquote.textContent = quote.quoteText;
+    $figcaption.textContent = quote.quoteAuthor;
 }
 document.addEventListener('DOMContentLoaded', getQuote);
 $btnQuote.addEventListener('click', getQuote);
 
 
 async function getWeather() {
-  let url = '';
-  if (localStorage.getItem('city') !== '') {
-    let cityLS = localStorage.getItem('city');
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${cityLS}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
-  }
-  else {
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${$city.textContent}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
-  }
-  const res = await fetch(url);
-  const data = await res.json();
+    let url = '';
+    if (localStorage.getItem('city') !== '') {
+        let cityLS = localStorage.getItem('city');
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${cityLS}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+    }
+    else {
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${$city.textContent}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+    }
+    const res = await fetch(url);
+    const data = await res.json();
 
- if (data.cod > 400) {
-  $weatherIcon.className = 'weather-icon owf';
-  $weatherIcon.classList.add(`owf-800`);
-  $temperature.textContent = `No `;
-  $weatherDescription.textContent = '[Enter right city name]';
+    if (data.cod > 400) {
+        $weatherIcon.className = 'weather-icon owf';
+        $weatherIcon.classList.add(`owf-800`);
+        $temperature.textContent = `No `;
+        $weatherDescription.textContent = '[Enter right city name]';
 
- }
-  $weatherIcon.className = 'weather-icon owf';
-  $weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-  $temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
-  $weatherDescription.textContent = data.weather[0].description;
+    }
+    $weatherIcon.className = 'weather-icon owf';
+    $weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    $temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+    $weatherDescription.textContent = data.weather[0].description;
 }
 
 function setCity(e) {
-  if (e.type === 'keypress') {
-    if (e.which == 13 || e.keyCode == 13) {
-      if (e.target.innerText === '') {
-        e.target.innerText = localStorage.getItem('city');
-        $city.innerText = localStorage.getItem('city');
-      }
-      else {
+    if (e.type === 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            if (e.target.innerText === '') {
+                e.target.innerText = localStorage.getItem('city');
+                $city.innerText = localStorage.getItem('city');
+            }
+            else {
+                localStorage.setItem('city', e.target.innerText);
+            }
+            getWeather();
+            $city.blur();
+        }
+    } else {
         localStorage.setItem('city', e.target.innerText);
-      }
-      getWeather();
-      $city.blur();
     }
-  } else {
-    localStorage.setItem('city', e.target.innerText);
-  }
 }
 
 document.addEventListener('DOMContentLoaded', getWeather);
